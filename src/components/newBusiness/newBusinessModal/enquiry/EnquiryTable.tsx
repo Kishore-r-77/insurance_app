7736ca0b65c3f4d7e804e44e1ae8185bf4aedbc1
Vengadesan <vengadesanvictory@gmail.com>
2023-04-03 14:41:58 +0530

    Fixed Limited display of GL Move

--- src/components/newBusiness/newBusinessModal/enquiry/EnquiryTable.tsx
+++ src/components/newBusiness/newBusinessModal/enquiry/EnquiryTable.tsx
@@ -2,9 +2,40 @@ import { Paper } from "@mui/material";
 import moment from "moment";
 import Table from "react-bootstrap/Table";
 import styles from "./enquiryTable.module.css";
+import InfoIcon from "@mui/icons-material/Info";
+import { useState } from "react";
+import GLAccountEnquiry from "./GLAccountEnquiry";
+import GLHistoryEnquiry from "./GLHistoryEnquiry";
 
-function EnquiryTable({ data, columns }: any) {
+function EnquiryTable({ data, columns, policyNo, infoOpen, historyOpen }: any) {
+  console.log(policyNo, "Policy No in Enq");
+  const [glEnquiry, setglEnquiry] = useState(false);
+  const [GLAcctNo, setGLAcctNo] = useState("");
+  const [contAmnt, setcontAmnt] = useState("");
+  const infoClickOpen = (value: any, value1: any) => {
+    console.log(value, "acct no value");
+    setGLAcctNo(value);
+    setcontAmnt(value1);
+    setglEnquiry(true);
+  };
+
+  const handleClickClose = () => {
+    setglEnquiry(false);
+  };
   console.log(data, "data");
+
+  const [glHistory, setglHistory] = useState(false);
+  const [Tranno, setTranno] = useState("");
+  const glhClickOpen = (value: any) => {
+    console.log(value, "tranno");
+    setTranno(value);
+    setglHistory(true);
+  };
+
+  const glhClickClose = () => {
+    setglHistory(false);
+  };
+
   return (
     <Paper className={styles.paperStyle}>
       <Table striped bordered hover>
@@ -38,12 +69,49 @@ function EnquiryTable({ data, columns }: any) {
                     </td>
                   );
                 }
-                return <td key={col.field}>{row[col.field]}</td>;
+                return (
+                  <>
+                    {infoOpen ? (
+                      <td
+                        key={col.field}
+                        onClick={() => infoClickOpen(row?.GlAccountno, row?.ContractAmount)}
+                      >
+                        {row[col.field]}
+                      </td>
+                    ): (
+                      historyOpen? (
+                        <td
+                        key={col.field}
+                        onClick={() => glhClickOpen(row?.Tranno)}
+                      >
+                        {row[col.field]}
+                      </td>
+                      ):(
+                        <td key={col.field}>{row[col.field]}</td>
+                      )
+                    )}
+                  </>
+                );
               })}
             </tr>
           ))}
         </tbody>
       </Table>
+
+      <GLAccountEnquiry
+        open={glEnquiry}
+        handleClose={handleClickClose}
+        policyNo={policyNo}
+        GLAccountNo={GLAcctNo}
+        contractAmnt={contAmnt}
+      />
+
+      <GLHistoryEnquiry
+        open={glHistory}
+        handleClose={glhClickClose}
+        policyNo={policyNo}
+        TransactionNo={Tranno}
+      />
     </Paper>
   );
 }
