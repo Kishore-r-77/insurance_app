5783e4dab6e803817d3ba27276d1de952e90596a
kishoreFuturaInsTech <kishore.r@futurainstech.com>
2023-06-16 13:41:10 +0530

    Integrated Quotation from Mr.Barath

---
+++ src/components/quotation/QHeaderQDetail.tsx
@@ -0,0 +1,413 @@
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import SearchIcon from "@mui/icons-material/Search";
+import { Button, MenuItem, TextField } from "@mui/material";
+import { useEffect, useReducer, useState } from "react";
+import CustomPagination from "../../utilities/Pagination/CustomPagination";
+import CustomTable from "../../utilities/Table/CustomTable";
+import { useAppSelector } from "../../redux/app/hooks";
+
+// ***  Attention : Check the import below and change it if required ***
+import { QHeaderStateType } from "../../reducerUtilities/types/quotation/qHeader/qHeaderTypes";
+
+import {
+  ACTIONS,
+  columns,
+  initialValues,
+} from "../../reducerUtilities/actions/quotation/qHeader/qHeaderActions";
+import styles from "./qHeaderqDetail.module.css";
+import {
+  addApi,
+  deleteApi,
+  editApi,
+  getAllApi,
+  getQheader,
+  getAllQDetailByQheaderApi,
+} from "./qHeaderApis/qHeaderApis";
+// import { editClickOpen } from "./qHeaderApis/qHeaderqDetailApis";
+//import QDetail from "../qDetail/QDetail";
+import QHeaderTable from "./qHeaderTable/QHeaderTable";
+import QHeaderQDetailModal from "./qHeaderqDetailModal/QHeaderQDetailModal";
+import QHeaderQDetailEnquiry from "./QHeaderQDetailEnquiry/QHeaderQDetailEnquiry";
+import axios from "axios";
+//import QHeaderQDetailModal  from "./QHeaderQDetailModal/QHeaderQDetailModal";
+//import QHeader FullModal from "./qHeaderFullModal/QHeaderFullModal";
+
+function QHeaderQDetail({ modalFunc, dataIndex, setNotify }: any) {
+  //data from getall api
+  const [data, setData] = useState([]);
+  //data got after rendering from table
+  const [record, setRecord] = useState<any>({});
+  //Reducer Function to be used inside UserReducer hook
+  const reducer = (state: QHeaderStateType, action: any) => {
+    switch (action.type) {
+      case ACTIONS.ONCHANGE:
+        return {
+          ...state,
+          [action.fieldName]: action.payload,
+        };
+      case ACTIONS.EDITCHANGE:
+        setRecord((prev: any) => ({
+          ...prev,
+          [action.fieldName]: action.payload,
+        }));
+        return {
+          ...state,
+          editOpen: true,
+        };
+      case ACTIONS.ADDOPEN:
+        return {
+          ...state,
+          addOpen: true,
+        };
+      case ACTIONS.EDITOPEN:
+        setRecord(action.payload);
+        return {
+          ...state,
+          editOpen: true,
+        };
+
+      case ACTIONS.INFOOPEN:
+        setRecord(action.payload);
+        return {
+          ...state,
+          infoOpen: true,
+        };
+
+      case ACTIONS.ADDCLOSE:
+        state = initialValues;
+        return {
+          ...state,
+          addOpen: false,
+        };
+
+      case ACTIONS.EDITCLOSE:
+        return {
+          ...state,
+          editOpen: false,
+        };
+      case ACTIONS.INFOCLOSE:
+        return {
+          ...state,
+          infoOpen: false,
+        };
+
+      // *** Attention: Check the Open /close ***
+      // case ACTIONS.QDETAILOPEN:
+      //   setRecord(action.payload);
+      //   return {
+      //     ...state,
+      //     qDetailOpen: true,
+      //   };
+      // case ACTIONS.QDETAILCLOSE:
+      //   return {
+      //     ...state,
+      //     qDetailOpen: false,
+      //   };
+      case ACTIONS.CLIENTOPEN:
+        return {
+          ...state,
+          clientOpen: true,
+        };
+      case ACTIONS.CLIENTCLOSE:
+        return {
+          ...state,
+          clientOpen: false,
+        };
+      case ACTIONS.ADDRESSOPEN:
+        return {
+          ...state,
+          addressOpen: true,
+        };
+      case ACTIONS.ADDRESSCLOSE:
+        return {
+          ...state,
+          addressOpen: false,
+        };
+      case ACTIONS.AGENCYOPEN:
+        return {
+          ...state,
+          agencyOpen: true,
+        };
+      case ACTIONS.AGENCYCLOSE:
+        return {
+          ...state,
+          agencyOpen: false,
+        };
+      case ACTIONS.CLIENTOPEN:
+        return {
+          ...state,
+          clientOpen: true,
+        };
+      case ACTIONS.CLIENTCLOSE:
+        return {
+          ...state,
+          clientOpen: false,
+        };
+
+      case ACTIONS.SORT_ASC:
+        const asc = !state.sortAsc;
+        if (state.sortDesc) {
+          state.sortDesc = false;
+        }
+        return {
+          ...state,
+          sortAsc: asc,
+          sortColumn: action.payload,
+        };
+      case ACTIONS.SORT_DESC:
+        const desc = !state.sortDesc;
+        if (state.sortAsc) {
+          state.sortAsc = false;
+        }
+        return {
+          ...state,
+          sortDesc: desc,
+          sortColumn: action.payload,
+        };
+      default:
+        return initialValues;
+    }
+  };
+
+  //Creating useReducer Hook
+  const [state, dispatch] = useReducer(reducer, initialValues);
+  const [pageNum, setpageNum] = useState(1);
+  const [pageSize, setpageSize] = useState(5);
+  const [totalRecords, settotalRecords] = useState(0);
+  const [isLast, setisLast] = useState(false);
+  const [fieldMap, setfieldMap] = useState([]);
+  const companyId = useAppSelector(
+    (state) => state.users.user.message.companyId
+  );
+  //Add Api
+  const handleFormSubmit = async () => {
+    const resp = addApi(state, companyId);
+
+    try {
+      dispatch({ type: ACTIONS.ADDCLOSE });
+      getData();
+      return resp;
+    } catch (err: any) {
+      err.message;
+    }
+  };
+
+  //Edit Api
+  const editFormSubmit = async () => {
+    editApi(record.ID)
+      .then((resp) => {
+        console.log(resp);
+        dispatch({ type: ACTIONS.EDITCLOSE });
+        getData();
+      })
+      .catch((err) => console.log(err.message));
+  };
+  console.log(record, "record");
+
+  //Hard Delete Api
+  const hardDelete = async (id: number) => {
+    deleteApi(id)
+      .then((resp) => {
+        console.log(resp);
+        getData();
+      })
+      .catch((err) => console.log(err.message));
+  };
+  //Get all Api
+  const getData = () => {
+    return getAllApi(pageNum, pageSize, state)
+      .then((resp) => {
+        console.log(resp);
+        // ***  Attention : Check the API and modify it, if required  ***
+        setData(resp.data["All QHeaders"]);
+        settotalRecords(resp.data.paginationData.totalRecords);
+        // ***  Attention : Check the API and modify it, if required   ***
+        setisLast(resp.data["All QHeaders"]?.length === 0);
+        setfieldMap(resp.data["Field Map"]);
+      })
+      .catch((err) => console.log(err.message));
+  };
+
+  //UseEffect Function to render data on Screen Based on Dependencies
+  useEffect(() => {
+    getData();
+    return () => {};
+  }, [pageNum, pageSize, state.sortAsc, state.sortDesc]);
+
+  const nexPage = () => {
+    setpageNum((prev) => prev + 1);
+  };
+
+  //Pagination Function to navigate to Previous page
+  const prevPage = () => {
+    if (pageNum > 1) {
+      setpageNum((prev) => prev - 1);
+    } else return;
+  };
+  const [qStatusCheckData, setQStatusCheckData] = useState<any>();
+  const handleForm = async () => {
+    try {
+      const response = await axios.post(
+        `http://localhost:3000/api/v1/quotationservices/qstatuscheck`,
+        {
+          CompanyID: parseInt(companyId),
+          QStatus: "",
+          QuoteDate: "20230402",
+          TranCode: "Q0003",
+        },
+        { withCredentials: true }
+      );
+      console.log(response);
+      setQStatusCheckData(initialValues);
+      setNotify({
+        isOpen: true,
+        message: "Status Checked",
+        type: "success",
+      });
+      return {
+        response,
+        status: response.status,
+      };
+    } catch (err: any) {
+      console.log(err);
+      return {
+        response: err,
+        status: err.response.status,
+      };
+    }
+  };
+  useEffect(() => {
+    handleForm();
+
+    return () => {};
+  }, []);
+
+  return (
+    <div>
+      <header className={styles.flexStyle}>
+        <span>
+          <TextField
+            select
+            value={state.searchCriteria}
+            placeholder="Search Criteria"
+            label="Search Criteria"
+            onChange={(e) =>
+              dispatch({
+                type: ACTIONS.ONCHANGE,
+                payload: e.target.value,
+                fieldName: "searchCriteria",
+              })
+            }
+            style={{ width: "12rem" }}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            {fieldMap.map((value: any) => (
+              <MenuItem key={value.fieldName} value={value.fieldName}>
+                {value.displayName}
+              </MenuItem>
+            ))}
+          </TextField>
+        </span>
+        <span className={styles["text-fields"]}>
+          <TextField
+            value={state.searchString}
+            placeholder="Search String"
+            label="Search String"
+            onChange={(e) =>
+              dispatch({
+                type: ACTIONS.ONCHANGE,
+                payload: e.target.value,
+                fieldName: "searchString",
+              })
+            }
+            style={{ width: "12rem" }}
+          />
+          <Button
+            variant="contained"
+            onClick={getData}
+            color="primary"
+            style={{
+              marginTop: "0.5rem",
+              maxWidth: "40px",
+              maxHeight: "40px",
+              minWidth: "40px",
+              minHeight: "40px",
+              backgroundColor: "#0a3161",
+              marginLeft: "10px",
+            }}
+          >
+            <SearchIcon />
+          </Button>
+        </span>
+        <h1>Quotation</h1>
+        <Button
+          id={styles["add-btn"]}
+          style={{
+            marginTop: "1rem",
+            maxWidth: "40px",
+            maxHeight: "40px",
+            minWidth: "40px",
+            minHeight: "40px",
+            backgroundColor: "#0a3161",
+          }}
+          variant="contained"
+          color="primary"
+          onClick={() => dispatch({ type: ACTIONS.ADDOPEN })}
+        >
+          <AddBoxIcon onClick={() => handleForm()} />
+        </Button>
+      </header>{" "}
+      <QHeaderTable
+        data={data}
+        getData={getData}
+        dataIndex={dataIndex}
+        modalFunc={modalFunc}
+        columns={columns}
+        ACTIONS={ACTIONS}
+        dispatch={dispatch}
+        hardDelete={hardDelete}
+        editForm={editApi}
+        initialValues={initialValues}
+        record={record}
+        //editClickOpen={editClickOpen}
+      />
+      <CustomPagination
+        pageNum={pageNum}
+        setpageSize={setpageSize}
+        // totalPages={totalPages}
+        totalRecords={totalRecords}
+        isLast={isLast}
+        prevPage={prevPage}
+        nexPage={nexPage}
+      />
+      <QHeaderQDetailModal
+        state={state}
+        ACTIONS={ACTIONS}
+        dispatch={dispatch}
+        getData={getData}
+        record={record}
+      />
+      <QHeaderQDetailEnquiry
+        state={state}
+        ACTIONS={ACTIONS}
+        dispatch={dispatch}
+        getData={getData}
+        record={record}
+        getQheader={getQheader}
+        getAllQDetail={getAllQDetailByQheaderApi}
+      />
+      {/* <CustomModal
+        open={state.qDetailOpen}
+        handleClose={() => dispatch({ type: ACTIONS.QDETAILCLOSE })}
+      >
+        <QDetail
+          qDetailByQHeaderData={qDetailByQHeaderData}
+          lookup={state.qDetailOpen}
+        />
+      </CustomModal> */}
+    </div>
+  );
+}
+export default QHeaderQDetail;
---
+++ src/components/quotation/qHeaderTable/QHeaderTable.tsx
@@ -0,0 +1,498 @@
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import InfoIcon from "@mui/icons-material/Info";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { IconButton, Paper } from "@mui/material";
+import Menu from "@mui/material/Menu";
+import moment from "moment";
+import { Button, MenuItem, TextField } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
+import Table from "react-bootstrap/Table";
+import styles from "./qHeaderTable.module.css";
+import AcceptModal from "../AcceptModal/Accept";
+import CalculateModal from "../CalculationModal/Calculation";
+import CancelModal from "../CancelModal/Cancel";
+import ValidateModal from "../ValidationModal/Validation";
+import PrintModal from "../PrintModal/Print";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useAppSelector } from "../../../redux/app/hooks";
+import { current } from "@reduxjs/toolkit";
+import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
+import { getQheader } from "../qHeaderApis/qHeaderApis";
+
+function QHeaderTable({
+  issueOpen,
+  confirmOpen,
+  data,
+  columns,
+  dispatch,
+  record,
+  ACTIONS,
+  sortParam,
+  getData,
+  hardDelete,
+  modalFunc,
+  initialValues,
+  setNotify,
+}: any) {
+  const [sort, setsort] = useState(
+    sortParam && sortParam.fieldName
+      ? sortParam
+      : { fieldName: columns[0].dbField, order: "asc" }
+  );
+
+  const companyId = useAppSelector(
+    (state) => state.users.user.message.companyId
+  );
+
+  const navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const openMenu = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>, id: any) => {
+    record.current = id;
+
+    setAnchorEl(event.currentTarget);
+    console.log(record.current, "----", record);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const [allowedMenuRecord, setAllowedMenuRecord] = useState<any>("");
+  const editClickOpen = (item: any, row: any) => {
+    setAllowedMenuRecord(item);
+    if (item.Action === "Change") {
+      dispatch({ type: ACTIONS.EDITOPEN, payload: row });
+    } else if (item.Action === "Validate") {
+      setValidateOpen(true);
+    } else if (item.Action === "Calculate") {
+      setCalculateOpen(true);
+    } else if (item.Action === "View") {
+      navigate("/qBenIllValue", {
+        state: { id: record.current },
+      });
+    } else if (item.Action === "Print") {
+      setPrintOpen(true);
+    } else if (item.Action === "Accept") {
+      setAcceptOpen(true);
+    } else if (item.Action === "Cancel") {
+      setCancelOpen(true);
+    }
+    handleClose();
+  };
+
+  const handleClickClose = () => {};
+
+  const [createQBenIllValuesData, setCreateQBenIllValuesData] = useState();
+  const createQBenIllValues = (ID: number) => {
+    axios
+      .post(
+        `http://localhost:3000/api/v1/quotationservices/qbenillvaluecreatebyqheader/${record.current}`,
+        {},
+        { withCredentials: true }
+      )
+      .then((resp) => {
+        console.log(resp);
+        handleClickClose();
+        setCreateQBenIllValuesData(initialValues);
+        setNotify({
+          isOpen: true,
+          message: "Created Successfully",
+          type: "success",
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const [statusCheckData, setStatusCheckData] = useState<any>();
+  const editForm = async (item: any, row: any) => {
+    try {
+      const response = await axios.post(
+        `http://localhost:3000/api/v1/quotationservices/qstatuscheck`,
+        {
+          CompanyID: parseInt(companyId),
+          QHeaderID: parseInt(record.current),
+          QStatus: qHeaderData.QStatus,
+          QuoteDate: "20230402",
+          TranCode: item.Trancode,
+        },
+        { withCredentials: true }
+      );
+      editClickOpen(item, row);
+      getData();
+      getQHeader();
+    } catch (err: any) {
+      console.log(err.message);
+      handleClose();
+    }
+  };
+  const [qHeaderData, setQHeaderData] = useState<any>([]);
+  const getQHeader = () => {
+    axios
+      .get(
+        `http://localhost:3000/api/v1/quotationservices/qheaderget/${record.current}`,
+        {
+          withCredentials: true,
+        }
+      )
+      .then((resp) => {
+        setQHeaderData(resp.data?.["QHeader"]);
+      })
+      .catch((err) => console.log(err.message));
+  };
+
+  const [calculateOpen, setCalculateOpen] = useState(false);
+  const handleCalculateOpen = (ID: any) => {
+    setCalculateOpen(true);
+    //setpolicyId(ID);
+    handleClose();
+  };
+  const handleCalculateClose = () => {
+    setCalculateOpen(false);
+  };
+
+  const [cancelOpen, setCancelOpen] = useState(false);
+  const handleCancelOpen = (ID: any) => {
+    setCancelOpen(true);
+    //setpolicyId(ID);
+    handleClose();
+  };
+  const handleCancelClose = () => {
+    setCancelOpen(false);
+  };
+
+  const [acceptOpen, setAcceptOpen] = useState(false);
+  const handleAcceptOpen = (ID: any) => {
+    setAcceptOpen(true);
+    //setpolicyId(ID);
+    handleClose();
+  };
+  const handleAcceptClose = () => {
+    setAcceptOpen(false);
+  };
+
+  const [printOpen, setPrintOpen] = useState(false);
+  const handlePrintOpen = (ID: any) => {
+    setPrintOpen(true);
+    //setpolicyId(ID);
+    handleClose();
+  };
+  const handlePrintClose = () => {
+    setPrintOpen(false);
+  };
+
+  const [validateOpen, setValidateOpen] = useState(false);
+  const handleValidateOpen = (ID: any) => {
+    setValidateOpen(true);
+    //setpolicyId(ID);
+    handleClose();
+  };
+  const handleValidateClose = () => {
+    setValidateOpen(false);
+  };
+
+  const [viewOpen, setViewOpen] = useState(false);
+  const handleViewOpen = (ID: any) => {
+    setViewOpen(true);
+    //setpolicyId(ID);
+    handleClose();
+  };
+  const handleViewClose = () => {
+    setViewOpen(false);
+  };
+
+  const [PostponeWithdrawnOpen, setPostponeWithdrawnOpen] = useState(false);
+  const handlePostponeWithdrawnOpen = (ID: any) => {
+    setPostponeWithdrawnOpen(true);
+    //setpolicyId(ID);
+    handleClose();
+  };
+  const handlePostponeWithdrawnClose = () => {
+    setPostponeWithdrawnOpen(false);
+  };
+
+  const [p0044SQMMData, setP0044SQMMData] = useState<any>();
+  const p0044SQMM = () => {
+    if (allowedMenuRecord.Action === "Calculate") {
+      createQBenIllValues(record.ID);
+    }
+    axios
+      .post(
+        `http://${allowedMenuRecord.URL}/${record.current}`,
+        {},
+        {
+          withCredentials: true,
+        }
+      )
+      .then((resp) => {
+        setP0044SQMMData(resp.data);
+        if (allowedMenuRecord.Action === "Validate") {
+          handleValidateClose();
+          handleClose();
+        } else if (allowedMenuRecord.Action === "Calculate") {
+          handleCalculateClose();
+        } else if (allowedMenuRecord.Action === "View") {
+          handleViewClose();
+        } else if (allowedMenuRecord.Action === "Print") {
+          handlePrintClose();
+        } else if (allowedMenuRecord.Action === "Accept") {
+          handleAcceptClose();
+        } else if (allowedMenuRecord.Action === "Cancel") {
+          handleCancelClose();
+        }
+        getData();
+        getQHeader();
+      })
+      .catch((err) => {
+        handleClose();
+        console.log(err.message);
+      });
+  };
+
+  const [allowedMenuData, setAllowedMenuData] = useState([]);
+  const getAllowedMenuData = () => {
+    axios
+      .get(
+        `http://localhost:3000/api/v1/basicservices/paramextradata?date=20220101`,
+        {
+          params: {
+            name: "P0044",
+            item: "SQMM",
+            company_id: companyId,
+          },
+          withCredentials: true,
+        }
+      )
+      .then((resp) => {
+        setAllowedMenuData(resp.data["AllowedMenus"]);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const [qCommunicationData, setQCommunicationData] = useState("");
+  const getQCommunicationByHeader = (row: number) => {
+    axios
+      .get(
+        `http://localhost:3000/api/v1/quotationservices/qcommidbyheader/${row}`,
+        {
+          withCredentials: true,
+        }
+      )
+      .then((resp) => {
+        setQCommunicationData(resp.data?.CommID);
+
+        // downloadQuotePdf(qCommunicationData);
+      })
+      .catch((err) => console.log(err.message));
+  };
+  console.log(qCommunicationData, "========");
+  const item = "QUOTE";
+  const downloadQuotePdf = (id: any) => {
+    axios
+      .get(
+        `http://localhost:3000/api/v1/basicservices/qgetReport?reportName=${item}&ID=${id}`,
+        {
+          withCredentials: true,
+          responseType: "blob",
+        }
+      )
+      .then((resp) => {
+        const url = window.URL.createObjectURL(new Blob([resp.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", `${item}.pdf`);
+        link.click();
+      })
+      .catch((err) => console.log(err.message));
+  };
+
+  useEffect(() => {
+    getAllowedMenuData();
+    p0044SQMM();
+
+    return () => {};
+  }, []);
+
+  useEffect(() => {
+    getQHeader();
+    return () => {};
+  }, [record.current]);
+  useEffect(() => {
+    downloadQuotePdf(qCommunicationData);
+
+    return () => {};
+  }, [qCommunicationData]);
+
+  return (
+    <Paper className={styles.paperStyle}>
+      <Table striped bordered hover>
+        <thead className={styles.header}>
+          <tr>
+            {columns?.map(
+              (
+                column: {
+                  field: string;
+                  header: string;
+                  dbField: string;
+                  sortable: boolean;
+                },
+                index: number
+              ) => (
+                <th key={column.dbField}>
+                  {column.header}
+                  {column?.sortable && (
+                    <span>
+                      <IconButton
+                        onClick={() => {
+                          setsort({
+                            fieldName: column.dbField,
+                            order: "asc",
+                          });
+
+                          return dispatch({
+                            type: ACTIONS.SORT_ASC,
+                            payload: column.dbField,
+                          });
+                        }}
+                      >
+                        <ArrowUpwardIcon
+                          className={
+                            sort.fieldName === column.dbField &&
+                            sort.order === "asc"
+                              ? styles.icon
+                              : styles["icon-disabled"]
+                          }
+                        />
+                      </IconButton>
+                      <IconButton
+                        onClick={() => {
+                          setsort({
+                            fieldName: column.dbField,
+                            order: "desc",
+                          });
+
+                          dispatch({
+                            type: ACTIONS.SORT_DESC,
+                            payload: column.dbField,
+                          });
+                        }}
+                      >
+                        <ArrowDownwardIcon
+                          className={
+                            sort.fieldName === column.dbField &&
+                            sort.order === "desc"
+                              ? styles.icon
+                              : styles["icon-disabled"]
+                          }
+                        />
+                      </IconButton>
+                    </span>
+                  )}
+                </th>
+              )
+            )}
+            {ACTIONS.EDITOPEN && <th>Actions</th>}
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((row: any) => (
+            <tr
+              onClick={() => modalFunc(row)}
+              key={row.ID}
+              className={styles["table-cell"]}
+            >
+              {columns.map((col: { field: string; type: string }) => {
+                if (col.type === "date") {
+                  return (
+                    <td key={col.field}>
+                      {moment(row[col.field]).format("DD-MM-YYYY")}
+                    </td>
+                  );
+                }
+                return <td key={col.field}>{row[col.field]}</td>;
+              })}
+              {ACTIONS.EDITOPEN && (
+                <td>
+                  <span className={styles.flexButtons}>
+                    <IconButton
+                      onClick={() =>
+                        dispatch({ type: ACTIONS.INFOOPEN, payload: row })
+                      }
+                    >
+                      {" "}
+                      <InfoIcon />
+                    </IconButton>
+
+                    <IconButton
+                      id="basic-button"
+                      aria-controls={openMenu ? "basic-menu" : undefined}
+                      aria-haspopup="true"
+                      aria-expanded={openMenu ? "true" : undefined}
+                      onClick={(e) => handleClick(e, row.ID)}
+                    >
+                      <MoreVertIcon />
+                    </IconButton>
+                    <IconButton
+                      onClick={() => getQCommunicationByHeader(row.ID)}
+                    >
+                      <PictureAsPdfIcon />
+                    </IconButton>
+                    <Menu
+                      id="basic-menu"
+                      anchorEl={anchorEl}
+                      open={openMenu}
+                      onClose={handleClose}
+                      MenuListProps={{
+                        "aria-labelledby": "basic-button",
+                      }}
+                    >
+                      {allowedMenuData.map((item: any) => (
+                        <MenuItem
+                          key={item.Action}
+                          onClick={() => editForm(item, row)}
+                        >
+                          {item.Action}
+                        </MenuItem>
+                      ))}
+                    </Menu>
+                  </span>
+                </td>
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+
+      <AcceptModal
+        open={acceptOpen}
+        handleClose={handleAcceptClose}
+        handleFormSubmit={p0044SQMM}
+      />
+      <CalculateModal
+        open={calculateOpen}
+        handleClose={handleCalculateClose}
+        handleFormSubmit={p0044SQMM}
+      />
+      <CancelModal
+        open={cancelOpen}
+        handleClose={handleCancelClose}
+        handleFormSubmit={p0044SQMM}
+      />
+      <PrintModal
+        open={printOpen}
+        handleClose={handlePrintClose}
+        handleFormSubmit={p0044SQMM}
+      />
+      <ValidateModal
+        open={validateOpen}
+        handleClose={handleValidateClose}
+        handleFormSubmit={p0044SQMM}
+      />
+    </Paper>
+  );
+}
+
+export default QHeaderTable;
