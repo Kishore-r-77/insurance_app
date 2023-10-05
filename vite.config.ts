a004714d349d3285bde28624835b5a0acd8204cc
Kishore-r-77 <kishorework77@gmail.com>
2023-10-05 17:37:11 +0530

    changed the vite configurations for hot reload

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // Your Vite configuration options go here
+  // Your Vite configuration
   plugins: [
     react({
       include: "**/*.tsx",
