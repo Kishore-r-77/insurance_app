7bd42c749450803c0449f0ad88704e47580ed2b0
Kishore-r-77 <kishorework77@gmail.com>
2023-10-05 17:39:03 +0530

    reverted the comment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // Your Vite configuration
+  // Your Vite configuration options go here
   plugins: [
     react({
       include: "**/*.tsx",
