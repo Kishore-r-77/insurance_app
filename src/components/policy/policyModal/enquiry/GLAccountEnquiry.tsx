9b8e56f8e5f170089de24797fc0f62491ac17a50
Kishore-r-77 <kishorework77@gmail.com>
2023-10-10 15:04:07 +0530

    deleted unused Global import

--- src/components/policy/policyModal/enquiry/GLAccountEnquiry.tsx
+++ src/components/policy/policyModal/enquiry/GLAccountEnquiry.tsx
@@ -1,9 +1,8 @@
-import { Global } from "@emotion/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import EnquiryTable from "./EnquiryTable";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import EnquiryTable from "./EnquiryTable";
 
 function GLAccountEnquiry({
   open,
