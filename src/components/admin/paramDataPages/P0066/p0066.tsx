f6fcd5a2c839d2a4993b166eef6c8fca6d7cb12e
Vengadesan <vengadesanvictory@gmail.com>
2023-10-25 09:54:49 +0530

    Made changes to Param P0066

--- src/components/admin/paramDataPages/P0066/p0066.tsx
+++ src/components/admin/paramDataPages/P0066/p0066.tsx
@@ -1,9 +1,6 @@
 import React, { forwardRef, useRef, useImperativeHandle, useEffect, useState } from "react";
 import { TextField, MenuItem, Checkbox, ListItemText } from "@mui/material";
-import AddBoxIcon from "@mui/icons-material/AddBox";
-import DeleteIcon from "@mui/icons-material/Delete";
-import Table from "react-bootstrap/Table";
-import CustomTooltip from "../../../../utilities/cutomToolTip/customTooltip";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import UserGroup from "../../usergroup/UserGroup";
 import useHttp from "../../../../hooks/use-http";
 import { getData } from "../../../../services/http-service";
@@ -13,69 +10,52 @@ import InfoIcon from "@mui/icons-material/Info";
 import  "./p0066.css";
 import P0066Enq  from "./p0066Enq";
 
-
 const P0066 = forwardRef((props: any, ref) => {
-
-  const {sendRequest : sendFlagRequest , status: getFlagResponseStatus ,  data: getFlagResponse , error:getFlagResponseError} = useHttp(getData, true); 
-
-  useEffect(() => {
-    let getDataParams:any = {}
-        getDataParams.companyId = 1;
-        getDataParams.languageId =  1;
-        getDataParams.seqno =  0;
-
-        getDataParams.name =  "P0050";
-
-        getDataParams.item = "FLAG";
-        sendFlagRequest({apiUrlPathSuffix : '/basicservices/paramItem' , getDataParams :getDataParams});
+  
 
 
-    },[]);
+  const nameRef: any = useRef();
+  const dialCodeRef: any = useRef();
+  const codeRef: any = useRef();
+  const flagRef: any = useRef();
 
-  const [inputdata, setInputdata] = useState(props.data ? props.data : {});
+  let inputdata: any = {};
+
+  if (props.data) {
+    inputdata = props.data;
+  }
+
+
   useImperativeHandle(ref, () => ({
     getData() {
-      let retData = inputdata;
-      retData.fieldList = retData.fieldList.filter(
-        (value: any) => value.name !== ""
-      );
+      inputdata.name = nameRef.current.value;
+      inputdata.dialCode = dialCodeRef.current.value;
+      inputdata.code = codeRef.current.value;
+      inputdata.flag = flagRef.current.value;
 
-      setInputdata((inputdata: any) => ({
-        ...inputdata,
-        fieldList: inputdata.fieldList.filter(
-          (value: any) => value.name !== ""
-        ),
-      }));
-      return retData;
+      return inputdata;
     },
   }));
 
-  const deleteItemHandler = (index: Number) => {
-    setInputdata((inputdata: any) => ({
-      ...inputdata,
-      fieldList: inputdata.fieldList.filter(
-        (_: any, ind: number) => ind !== index
-      ),
-    }));
-  };
+  const getHTML =()=>{
+    fetch(`/p0066.html`)
+      .then(response => response.text())
+      .then(content => setHtmlContent(content))
+      .catch(error => console.error('Error fetching HTML file:', error));
+  }
 
-  const fieldChangeHandler = (index: number, fieldname: string, value: any, isnumber: boolean) => {
-    setInputdata((inputdata: any) => ({
-      ...inputdata,
-      fieldList: inputdata.fieldList.map((val: any, ind: number) => {
-        if (index === ind) {
-          if (isnumber){
-            val[fieldname] = Number(value);
-          }
-          else{
-            val[fieldname] = value;
-          }
-                    return val;
-        } else {
-          return val;
-        }
-      }),
-    }));
+  const [htmlContent, setHtmlContent] = useState('');
+  
+  useEffect(() => {
+    getHTML();
+    return () => {}
+  }, [])
+
+  const [showHtmlContent, setShowHtmlContent] = useState(false);
+
+  const toggleHtmlContent = () => {
+    getHTML();
+    setShowHtmlContent(!showHtmlContent);
   };
 
   const [enq, setEnq] = useState(false)
@@ -87,184 +67,79 @@ const P0066 = forwardRef((props: any, ref) => {
   const enqClose = () =>{
     setEnq(false)
   }
-
+  
   return (
     <>
     <InfoIcon
-      onClick={() => enqOpen()} />
-	  
-    <Table striped bordered hover>
-      <thead
-        style={{
-          backgroundColor: "rgba(71, 11, 75, 1)",
-          color: "white",
-          position: "sticky",
-          top: "0",
-        }}
-      >
+        onClick={() =>enqOpen()}
+      />
+      <Grid2 xs={12} md={6} lg={4} sm={6} xl={4}>
+        <TextField
+          
+          inputProps={{
+            readOnly: props.mode === "display" || props.mode === "delete",
+          }}
+          id="name"
+          name="name"
+          inputRef={nameRef}
+          placeholder="Name"
+          label="Name"
+          defaultValue={inputdata.name}
+          fullWidth
+          margin="dense"
+        />
+        </Grid2>
 
-        <tr>
-          <th>Name</th> 
-          <th>Dial Code</th> 
-          <th>Code</th> 
-          <th>flag</th> 
-          {(props.mode === "update" || props.mode === "create") && 
-            inputdata.fieldList?.length > 0 && <th>Actions</th>}
-          {(props.mode === "update" || props.mode === "create") &&
-            (!inputdata.fieldList || inputdata.fieldList?.length === 0) && (
-              <th>
-                <CustomTooltip text="Add">
-                  <AddBoxIcon
-                    onClick={() => {
-                      setInputdata((inputdata: any) => ({
-                        ...inputdata,
-                        fieldList: [
-                          {
-                            name: "",
-                            dialCode: "",
-                            code: "",
-                            flag: "",
-                          },
-                        ],
-                      }));
-                    }}
-                  />
-                </CustomTooltip>
-              </th>
-            )}
-        </tr>
-      </thead>
-      <tbody>
-        {inputdata.fieldList?.map((value: any, index: number) => (
-          <tr key={index}>
-            <td>
-              <TextField
-                inputProps={{
-                readOnly: props.mode === "display" || props.mode === "delete",
-                }}
-                id="name"
-                name="name"
-                value={value.name}
-                onChange={(e) =>
-                  fieldChangeHandler(index, "name", e.target.value,false)
-                }
-                fullWidth
-                size="small"
-                type="text"
-                margin="dense"
-              />
-            </td>
+      <Grid2 xs={12} md={6} lg={4} sm={6} xl={4}>
+        <TextField
+          
+          inputProps={{
+            readOnly: props.mode === "display" || props.mode === "delete",
+          }}
+          id="dialCode"
+          name="dialCode"
+          inputRef={dialCodeRef}
+          placeholder="Dial Code"
+          label="Dial Code"
+          defaultValue={inputdata.dialCode}
+          fullWidth
+          margin="dense"
+        />
+        </Grid2>
 
-            <td>
-              <TextField
-                inputProps={{
-                readOnly: props.mode === "display" || props.mode === "delete",
-                }}
-                id="dialCode"
-                name="dialCode"
-                value={value.dialCode}
-                onChange={(e) =>
-                  fieldChangeHandler(index, "dialCode", e.target.value,false)
-                }
-                fullWidth
-                size="small"
-                type="text"
-                margin="dense"
-              />
-            </td>
+      <Grid2 xs={12} md={6} lg={4} sm={6} xl={4}>
+        <TextField
+          
+          inputProps={{
+            readOnly: props.mode === "display" || props.mode === "delete",
+          }}
+          id="code"
+          name="code"
+          inputRef={codeRef}
+          placeholder="Code"
+          label="Code"
+          defaultValue={inputdata.code}
+          fullWidth
+          margin="dense"
+        />
+        </Grid2>
 
-            <td>
-              <TextField
-                inputProps={{
-                readOnly: props.mode === "display" || props.mode === "delete",
-                }}
-                id="code"
-                name="code"
-                value={value.code}
-                onChange={(e) =>
-                  fieldChangeHandler(index, "code", e.target.value,false)
-                }
-                fullWidth
-                size="small"
-                type="text"
-                margin="dense"
-              />
-            </td>
-
-            <td>
-              <TextField
-                select
-                inputProps={{
-                readOnly: props.mode === "display" || props.mode === "delete",
-                }}
-                id="flag"
-                name="flag"
-                value={value.flag}
-                onChange={(e) =>
-                  fieldChangeHandler(index, "flag", e.target.value,false)
-                }
-                fullWidth
-                size="small"
-                type="text"
-                margin="dense"
-                SelectProps={{
-                  multiple: false,
-                }}
-              >
-                {getFlagResponse?.param.data.dataPairs.map((value:any) => (
-                  <MenuItem key={value.code} value={value.code}>
-                {value.code} - {value.description}
-                  </MenuItem>
-                ))}
-              </TextField>
-          </td>
-
-            {(props.mode === "update" || props.mode === "create") && (
-              <td>
-                <span
-                  style={{
-                    display: "flex",
-                    gap: "0.5rem",
-                    marginTop: "0.9rem",
-                  }}
-                >
-                  <CustomTooltip text="Remove">
-                    <DeleteIcon
-                      color="error"
-                      onClick={() => {
-                        deleteItemHandler(index);
-                      }}
-                    />
-
-                  </CustomTooltip>
-                  {index === inputdata.fieldList.length - 1 && (
-                    <CustomTooltip text="Add">
-                      <AddBoxIcon
-                        onClick={() => {
-                          setInputdata((inputdata: any) => ({
-                            ...inputdata,
-                            fieldList: [
-                              ...inputdata.fieldList,
-                              {
-                                name: "",
-                                dialCode: "",
-                                code: "",
-                                flag: "",
-
-                              },
-                            ],
-                          }));
-                        }}
-                      />
-                    </CustomTooltip>
-                  )}
-                </span>
-              </td>
-            )}
-          </tr>
-        ))}
-      </tbody>
-    </Table>
+      <Grid2 xs={12} md={6} lg={4} sm={6} xl={4}>
+        <TextField
+          
+          inputProps={{
+            readOnly: props.mode === "display" || props.mode === "delete",
+          }}
+          id="flag"
+          name="flag"
+          inputRef={flagRef}
+          placeholder="Flag"
+          label="Flag"
+          defaultValue={inputdata.flag}
+          fullWidth
+          margin="dense"
+        />
+        </Grid2>
 
 
         <P0066Enq
--- src/components/admin/paramDataPages/P0066/p0066Enq.tsx
+++ src/components/admin/paramDataPages/P0066/p0066Enq.tsx
@@ -1,254 +1,29 @@
-import React, { forwardRef, useRef, useImperativeHandle, useEffect, useState } from "react";
-import { TextField, MenuItem, Checkbox, ListItemText } from "@mui/material";
-import AddBoxIcon from "@mui/icons-material/AddBox";
-import DeleteIcon from "@mui/icons-material/Delete";
-import Table from "react-bootstrap/Table";
-import CustomTooltip from "../../../../utilities/cutomToolTip/customTooltip";
-import UserGroup from "../../usergroup/UserGroup";
-import useHttp from "../../../../hooks/use-http";
-import { getData } from "../../../../services/http-service";
+import React, { forwardRef, useEffect, useState } from 'react'
+import CustomModal from '../../../../utilities/modal/CustomModal';
 
-import InfoIcon from "@mui/icons-material/Info";
+const P0066Enq = forwardRef(({open, handleClose}: any) => {
+    const [htmlContent, setHtmlContent] = useState('');
+    const getHTML =()=>{
+      fetch(`/p0066.html`)
 
-import  "./p0066.css";
-import P0066Enq  from "./p0066Enq";
-
-
-const P0066 = forwardRef((props: any, ref) => {
-  const [inputdata, setInputdata] = useState(props.data ? props.data : {});
-  useImperativeHandle(ref, () => ({
-    getData() {
-      let retData = inputdata;
-      retData.fieldList = retData.fieldList.filter(
-        (value: any) => value.name !== ""
-      );
-
-      setInputdata((inputdata: any) => ({
-        ...inputdata,
-        fieldList: inputdata.fieldList.filter(
-          (value: any) => value.name !== ""
-        ),
-      }));
-      return retData;
-    },
-  }));
-
-  const deleteItemHandler = (index: Number) => {
-    setInputdata((inputdata: any) => ({
-      ...inputdata,
-      fieldList: inputdata.fieldList.filter(
-        (_: any, ind: number) => ind !== index
-      ),
-    }));
-  };
-
-  const fieldChangeHandler = (index: number, fieldname: string, value: any, isnumber: boolean) => {
-    setInputdata((inputdata: any) => ({
-      ...inputdata,
-      fieldList: inputdata.fieldList.map((val: any, ind: number) => {
-        if (index === ind) {
-          if (isnumber){
-            val[fieldname] = Number(value);
-          }
-          else{
-            val[fieldname] = value;
-          }
-                    return val;
-        } else {
-          return val;
-        }
-      }),
-    }));
-  };
-
-  const [enq, setEnq] = useState(false)
-
-  const enqOpen = () =>{
-    setEnq(true)
+      .then(response => response.text())
+      .then(content => setHtmlContent(content))
+      .catch(error => console.error('Error fetching HTML file:', error));
   }
 
-  const enqClose = () =>{
-    setEnq(false)
-  }
-
+  useEffect(() => {
+    getHTML();
+    return () => {}
+  }, [])
+  
   return (
-    <>
-    <InfoIcon
-      onClick={() => enqOpen()} />
-	  
-    <Table striped bordered hover>
-      <thead
-        style={{
-          backgroundColor: "rgba(71, 11, 75, 1)",
-          color: "white",
-          position: "sticky",
-          top: "0",
-        }}
-      >
-
-        <tr>
-          <th>Name</th> 
-          <th>Dial Code</th> 
-          <th>Code</th> 
-          <th>Flag</th> 
-          {(props.mode === "update" || props.mode === "create") && 
-            inputdata.fieldList?.length > 0 && <th>Actions</th>}
-          {(props.mode === "update" || props.mode === "create") &&
-            (!inputdata.fieldList || inputdata.fieldList?.length === 0) && (
-              <th>
-                <CustomTooltip text="Add">
-                  <AddBoxIcon
-                    onClick={() => {
-                      setInputdata((inputdata: any) => ({
-                        ...inputdata,
-                        fieldList: [
-                          {
-                            name: "",
-                            dialCode: "",
-                            code: "",
-                            flag: "",
-                          },
-                        ],
-                      }));
-                    }}
-                  />
-                </CustomTooltip>
-              </th>
-            )}
-        </tr>
-      </thead>
-      <tbody>
-        {inputdata.fieldList?.map((value: any, index: number) => (
-          <tr key={index}>
-            <td>
-              <TextField
-                inputProps={{
-                readOnly: props.mode === "display" || props.mode === "delete",
-                }}
-                id="name"
-                name="name"
-                value={value.name}
-                onChange={(e) =>
-                  fieldChangeHandler(index, "name", e.target.value,false)
-                }
-                fullWidth
-                size="small"
-                type="text"
-                margin="dense"
-              />
-            </td>
-
-            <td>
-              <TextField
-                inputProps={{
-                readOnly: props.mode === "display" || props.mode === "delete",
-                }}
-                id="dialCode"
-                name="dialCode"
-                value={value.dialCode}
-                onChange={(e) =>
-                  fieldChangeHandler(index, "dialCode", e.target.value,false)
-                }
-                fullWidth
-                size="small"
-                type="text"
-                margin="dense"
-              />
-            </td>
-
-            <td>
-              <TextField
-                inputProps={{
-                readOnly: props.mode === "display" || props.mode === "delete",
-                }}
-                id="code"
-                name="code"
-                value={value.code}
-                onChange={(e) =>
-                  fieldChangeHandler(index, "code", e.target.value,false)
-                }
-                fullWidth
-                size="small"
-                type="text"
-                margin="dense"
-              />
-            </td>
-
-            <td>
-              <TextField
-                inputProps={{
-                readOnly: props.mode === "display" || props.mode === "delete",
-                }}
-                id="flag"
-                name="flag"
-                value={value.flag}
-                onChange={(e) =>
-                  fieldChangeHandler(index, "flag", e.target.value,false)
-                }
-                fullWidth
-                size="small"
-                type="text"
-                margin="dense"
-              />
-            </td>
-
-            {(props.mode === "update" || props.mode === "create") && (
-              <td>
-                <span
-                  style={{
-                    display: "flex",
-                    gap: "0.5rem",
-                    marginTop: "0.9rem",
-                  }}
-                >
-                  <CustomTooltip text="Remove">
-                    <DeleteIcon
-                      color="error"
-                      onClick={() => {
-                        deleteItemHandler(index);
-                      }}
-                    />
-
-                  </CustomTooltip>
-                  {index === inputdata.fieldList.length - 1 && (
-                    <CustomTooltip text="Add">
-                      <AddBoxIcon
-                        onClick={() => {
-                          setInputdata((inputdata: any) => ({
-                            ...inputdata,
-                            fieldList: [
-                              ...inputdata.fieldList,
-                              {
-                                name: "",
-                                dialCode: "",
-                                code: "",
-                                flag: "",
-
-                              },
-                            ],
-                          }));
-                        }}
-                      />
-                    </CustomTooltip>
-                  )}
-                </span>
-              </td>
-            )}
-          </tr>
-        ))}
-      </tbody>
-    </Table>
-
-
-        <P0066Enq
-        open={enq}
-        handleClose={enqClose}
-
-        />
-
-    </>
-  );
+    <div>
+        <CustomModal open={open} handleClose={handleClose}>
+        <div dangerouslySetInnerHTML={{ __html: htmlContent }}></div>
+        </CustomModal>
+    </div>
+  )
 });
 
-export default P0066;
 
+export default P0066Enq;
