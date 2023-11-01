94a29091f522e2cc4b6f4cb1a39254095851130d
shubhampatilfutura <shubham.patil@futurainstech.com>
2023-11-01 13:27:42 +0530

    remove unused import from freq change modal

--- src/components/policyUploadDemo/freqChangeModal/FreqChangeModal.tsx
+++ src/components/policyUploadDemo/freqChangeModal/FreqChangeModal.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import CustomModal from "../../../utilities/modal/CustomModal";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import { MenuItem, TextField } from "@mui/material";
 import axios from "axios";
