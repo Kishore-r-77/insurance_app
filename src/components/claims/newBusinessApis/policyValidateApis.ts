2c94f896bfd9caefefed6c7f9f39195f94a2055c
Kishore-r-77 <kishorework77@gmail.com>
2023-11-01 11:45:58 +0530

    fixed path in policyValidate apis

--- src/components/claims/newBusinessApis/policyValidateApis.ts
+++ src/components/claims/newBusinessApis/policyValidateApis.ts
@@ -1,27 +1,26 @@
 import axios from "axios";
 import moment from "moment";
-//Attention: Check the path below and change it if required 
-import { PolicyValidateStateType } from "./policyValidateTypes";
-
+import { PolicyValidateStateType } from "../../../reducerUtilities/types/validatepolicy/policyValidateTypes";
 
 export const getAllApi = (
-pageNum: number,
-pageSize: number,
-state: PolicyValidateStateType
-)=> {
-// Attention : Check and update the below API, if required
+  pageNum: number,
+  pageSize: number,
+  state: PolicyValidateStateType
+) => {
   return axios.get(
-`http://localhost:3000/api/v1/basicservices/getallpolicyValidates`, {
-                withCredentials: true,
-    params: {
-      pageNum: pageNum,
-      pageSize: pageSize,
-      searchString: state.searchString,
-      searchCriteria: state.searchCriteria,
-      sortColumn: state.sortColumn,
-      sortDirection: state.sortAsc ? "asc" : state.sortDesc ? "desc" : null,
-    },
-  });
+    `http://localhost:3000/api/v1/basicservices/getallpolicyValidates`,
+    {
+      withCredentials: true,
+      params: {
+        pageNum: pageNum,
+        pageSize: pageSize,
+        searchString: state.searchString,
+        searchCriteria: state.searchCriteria,
+        sortColumn: state.sortColumn,
+        sortDirection: state.sortAsc ? "asc" : state.sortDesc ? "desc" : null,
+      },
+    }
+  );
 };
 export const paramItem = (
   companyId: number,
@@ -38,14 +37,14 @@ export const paramItem = (
   });
 };
 
-  export const addApi = (state: PolicyValidateStateType, companyId: number) => { 
-// Attention : Check and update the below API, if required
+export const addApi = (state: PolicyValidateStateType, companyId: number) => {
+  // Attention : Check and update the below API, if required
   return axios.post(
-  `http://localhost:3000/api/v1/basicservices/policyValidatecreate`, 
-    { 
+    `http://localhost:3000/api/v1/basicservices/policyValidatecreate`,
+    {
       PolicyID: parseInt(state.PolicyID),
       CompanyID: companyId,
-      Coverage : state.Coverage ,
+      Coverage: state.Coverage,
       BPrem: state.BPrem,
       CovrGst: state.CovrGst,
       CovrStampduty: state.CovrStampduty,
@@ -56,14 +55,14 @@ export const paramItem = (
   );
 };
 
-  export const editApi = (record: any) => { 
-// Attention : Check and update the below API, if required
+export const editApi = (record: any) => {
+  // Attention : Check and update the below API, if required
   return axios.put(
-  `http://localhost:3000/api/v1/basicservices/policyValidateupdate`, 
-    { 
+    `http://localhost:3000/api/v1/basicservices/policyValidateupdate`,
+    {
       PolicyID: parseInt(record.PolicyID),
-      CompanyID:  parseInt(record.CompanyID),
-      Coverage : record.Coverage ,
+      CompanyID: parseInt(record.CompanyID),
+      Coverage: record.Coverage,
       BPrem: record.BPrem,
       CovrGst: record.CovrGst,
       CovrStampduty: record.CovrStampduty,
@@ -76,8 +75,8 @@ export const paramItem = (
 
 export const deleteApi = (id: number) => {
   return axios.delete(
-//Attention: Check the path below,if required 
-    `http://localhost:3000/api/v1/basicservices/policyValidatedelete/${id}` ,
+    //Attention: Check the path below,if required
+    `http://localhost:3000/api/v1/basicservices/policyValidatedelete/${id}`,
     {
       withCredentials: true,
     }
