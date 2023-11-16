daa3d673144fa95f3ee503fc9216d07f13ecc8c7
Kishore-r-77 <kishorework77@gmail.com>
2023-11-16 20:06:08 +0530

    code redundancy has been removed by taking of unused imports in ResultModal.tsx

--- src/components/csmm/freqChangeModal/ResultModal.tsx
+++ src/components/csmm/freqChangeModal/ResultModal.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-import CustomModal from "../../../utilities/modal/CustomModal";
 import { TextField } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import CustomModal from "../../../utilities/modal/CustomModal";
 
 function ResultModal({ open, handleClose, record, completed }: any) {
   const size: string = "";
@@ -19,57 +18,57 @@ function ResultModal({ open, handleClose, record, completed }: any) {
         ) : (
           <Grid2 container spacing={2}>
             <Grid2 lg={4}>
-            <TextField
-              id="No Of Dues"
-              name="No Of Dues"
-              value={record?.NoOfDues}
-              placeholder="No Of Dues"
-              label="No Of Dues"
-              fullWidth
-              inputProps={{ readOnly: true }}
-              InputLabelProps={{ shrink: true }}
-              margin="dense"
-            ></TextField>
-          </Grid2>
-          <Grid2 lg={4}>
-            <TextField
-              id="Total Premium"
-              name="Total Premium"
-              value={record?.TotalPrem}
-              placeholder="Total Premium"
-              label="Total Premium"
-              fullWidth
-              inputProps={{ readOnly: true }}
-              InputLabelProps={{ shrink: true }}
-              margin="dense"
-            ></TextField>
-          </Grid2>
-          <Grid2 lg={4}>
-            <TextField
-              id="GST"
-              name="GST"
-              value={record?.TotalGST}
-              placeholder="GST"
-              label="GST"
-              fullWidth
-              inputProps={{ readOnly: true }}
-              InputLabelProps={{ shrink: true }}
-              margin="dense"
-            ></TextField>
-          </Grid2>
-          <Grid2 lg={4}>
-            <TextField
-              id="Total Amount"
-              name="Total Amount"
-              value={record?.TotalAmount}
-              placeholder="Total Amount"
-              label="Total Amount"
-              fullWidth
-              inputProps={{ readOnly: true }}
-              InputLabelProps={{ shrink: true }}
-              margin="dense"
-            ></TextField>
-          </Grid2>
+              <TextField
+                id="No Of Dues"
+                name="No Of Dues"
+                value={record?.NoOfDues}
+                placeholder="No Of Dues"
+                label="No Of Dues"
+                fullWidth
+                inputProps={{ readOnly: true }}
+                InputLabelProps={{ shrink: true }}
+                margin="dense"
+              ></TextField>
+            </Grid2>
+            <Grid2 lg={4}>
+              <TextField
+                id="Total Premium"
+                name="Total Premium"
+                value={record?.TotalPrem}
+                placeholder="Total Premium"
+                label="Total Premium"
+                fullWidth
+                inputProps={{ readOnly: true }}
+                InputLabelProps={{ shrink: true }}
+                margin="dense"
+              ></TextField>
+            </Grid2>
+            <Grid2 lg={4}>
+              <TextField
+                id="GST"
+                name="GST"
+                value={record?.TotalGST}
+                placeholder="GST"
+                label="GST"
+                fullWidth
+                inputProps={{ readOnly: true }}
+                InputLabelProps={{ shrink: true }}
+                margin="dense"
+              ></TextField>
+            </Grid2>
+            <Grid2 lg={4}>
+              <TextField
+                id="Total Amount"
+                name="Total Amount"
+                value={record?.TotalAmount}
+                placeholder="Total Amount"
+                label="Total Amount"
+                fullWidth
+                inputProps={{ readOnly: true }}
+                InputLabelProps={{ shrink: true }}
+                margin="dense"
+              ></TextField>
+            </Grid2>
           </Grid2>
         )}
       </CustomModal>
