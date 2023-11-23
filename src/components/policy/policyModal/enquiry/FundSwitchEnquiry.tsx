68f42afe3c94dad4af6d51e7c09d71b5010583f5
Kishore-r-77 <kishorework77@gmail.com>
2023-11-23 11:59:41 +0530

    completed fundswitch enquiry

--- src/components/policy/policyModal/enquiry/FundSwitchEnquiry.tsx
+++ src/components/policy/policyModal/enquiry/FundSwitchEnquiry.tsx
@@ -14,6 +14,8 @@ import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import CustomFullModal from "../../../../utilities/modal/CustomFullModal";
 import moment from "moment";
+import styles from "./fundSwitchEnquiry.module.css";
+import CustomModal from "../../../../utilities/modal/CustomModal";
 
 export default function FundSwitchEnquiry({
   open: fundOpen,
@@ -22,7 +24,8 @@ export default function FundSwitchEnquiry({
 }: any) {
   const [open, setOpen] = React.useState(false);
   return (
-    <CustomFullModal
+    <CustomModal
+      size="xl"
       open={fundOpen}
       handleClose={handleClose}
       title="Ilp Switch Detail"
@@ -30,17 +33,20 @@ export default function FundSwitchEnquiry({
       <TableContainer component={Paper}>
         <h2 style={{ textAlign: "center" }}>Ilp Switch Header</h2>
         <Table aria-label="collapsible table">
-          <TableHead>
+          <TableHead className={styles["table-head"]}>
             <TableRow>
               <TableCell />
-              <TableCell>PolicyID</TableCell>
-              <TableCell>BenefitID</TableCell>
-              <TableCell>EffectiveDate</TableCell>
-              <TableCell>FundSwitchBasis</TableCell>
+              <TableCell className={styles.thstyle}>PolicyID</TableCell>
+              <TableCell className={styles.thstyle}>BenefitID</TableCell>
+              <TableCell className={styles.thstyle}>EffectiveDate</TableCell>
+              <TableCell className={styles.thstyle}>FundSwitchBasis</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
+            <TableRow
+              sx={{ "& > *": { borderBottom: "unset" } }}
+              style={{ backgroundColor: "#f0f8ff" }}
+            >
               <TableCell>
                 <IconButton
                   aria-label="expand row"
@@ -50,12 +56,14 @@ export default function FundSwitchEnquiry({
                   {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
                 </IconButton>
               </TableCell>
-              <TableCell>{row?.PolicyID}</TableCell>
-              <TableCell>{row?.BenefitID}</TableCell>
-              <TableCell>
+              <TableCell className={styles.tdstyle}>{row?.PolicyID}</TableCell>
+              <TableCell className={styles.tdstyle}>{row?.BenefitID}</TableCell>
+              <TableCell className={styles.tdstyle}>
                 {moment(row?.EffectiveDate).format("DD-MM-YYYY")}
               </TableCell>
-              <TableCell>{row?.FundSwitchBasis}</TableCell>
+              <TableCell className={styles.tdstyle}>
+                {row?.FundSwitchBasis}
+              </TableCell>
             </TableRow>
             <TableRow>
               <TableCell
@@ -73,37 +81,81 @@ export default function FundSwitchEnquiry({
                       Ilp Switch Funds
                     </Typography>
                     <Table size="small" aria-label="purchases">
-                      <TableHead>
+                      <TableHead className={styles["table-head"]}>
                         <TableRow>
-                          <TableCell>Policy Id</TableCell>
-                          <TableCell>Benefit Id</TableCell>
-                          <TableCell>EffectiveDate</TableCell>
-                          <TableCell>SwitchDirection</TableCell>
-                          <TableCell>Fund Code</TableCell>
-                          <TableCell>Fund Type</TableCell>
-                          <TableCell>FundCurr</TableCell>
-                          <TableCell align="right">FundPrice</TableCell>
+                          <TableCell className={styles.thstyle}>
+                            Policy Id
+                          </TableCell>
+                          <TableCell className={styles.thstyle}>
+                            Benefit Id
+                          </TableCell>
+                          <TableCell className={styles.thstyle}>
+                            EffectiveDate
+                          </TableCell>
+                          <TableCell className={styles.thstyle}>
+                            SwitchDirection
+                          </TableCell>
+                          <TableCell className={styles.thstyle}>
+                            Fund Code
+                          </TableCell>
+                          <TableCell className={styles.thstyle}>
+                            Fund Type
+                          </TableCell>
+                          <TableCell className={styles.thstyle}>
+                            FundCurr
+                          </TableCell>
+                          <TableCell className={styles.thstyle} align="right">
+                            FundPrice
+                          </TableCell>
                         </TableRow>
                       </TableHead>
                       <TableBody>
-                        {row?.IlpSwitchFunds?.map((funds: any) => (
-                          <TableRow key={funds.PolicyID}>
-                            <TableCell component="th" scope="row">
-                              {funds.PolicyID}
-                            </TableCell>
-                            <TableCell>{funds.BenefitID}</TableCell>
-                            <TableCell>
-                              {moment(funds.EffectiveDate).format("DD-MM-YYYY")}
-                            </TableCell>
-                            <TableCell>{funds.SwitchDirection}</TableCell>
-                            <TableCell>{funds.FundCode}</TableCell>
-                            <TableCell>{funds.FundType}</TableCell>
-                            <TableCell>{funds.FundCurr}</TableCell>
-                            <TableCell align="right">
-                              {funds.FundPrice}
-                            </TableCell>
-                          </TableRow>
-                        ))}
+                        {row?.IlpSwitchFunds?.map(
+                          (funds: any, index: number) => (
+                            <TableRow
+                              key={funds.PolicyID}
+                              style={{
+                                background:
+                                  index % 2 === 0 ? "#f0f8ff" : "#d3e0ea", // Light blue for even rows, slightly darker blue for odd rows
+                                transition: "background 0.3s ease-in-out", // Add a smooth transition effect
+                              }}
+                            >
+                              <TableCell
+                                className={styles.tdstyle}
+                                component="th"
+                                scope="row"
+                              >
+                                {funds.PolicyID}
+                              </TableCell>
+                              <TableCell className={styles.tdstyle}>
+                                {funds.BenefitID}
+                              </TableCell>
+                              <TableCell>
+                                {moment(funds.EffectiveDate).format(
+                                  "DD-MM-YYYY"
+                                )}
+                              </TableCell>
+                              <TableCell className={styles.tdstyle}>
+                                {funds.SwitchDirection}
+                              </TableCell>
+                              <TableCell className={styles.tdstyle}>
+                                {funds.FundCode}
+                              </TableCell>
+                              <TableCell className={styles.tdstyle}>
+                                {funds.FundType}
+                              </TableCell>
+                              <TableCell className={styles.tdstyle}>
+                                {funds.FundCurr}
+                              </TableCell>
+                              <TableCell
+                                className={styles.tdstyle}
+                                align="right"
+                              >
+                                {funds.FundPrice}
+                              </TableCell>
+                            </TableRow>
+                          )
+                        )}
                       </TableBody>
                     </Table>
                   </Box>
@@ -113,6 +165,6 @@ export default function FundSwitchEnquiry({
           </TableBody>
         </Table>
       </TableContainer>
-    </CustomFullModal>
+    </CustomModal>
   );
 }
