9d8c6c5ecb78bac405dd067ce3abe8393bce4555
yukeshFuturainsTech <yukesh.jayaseelan@futurainstech@gmail.com>
2023-12-11 14:53:34 +0530

    sachange enquiry table

--- src/components/policy/policyModal/enquiry/SAChangeEnquiry.tsx
+++ src/components/policy/policyModal/enquiry/SAChangeEnquiry.tsx
@@ -3,91 +3,103 @@ import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import EnquiryTable from "./EnquiryTable";
+import CustomModal from "../../../../utilities/modal/CustomModal";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import moment from "moment";
+import styles from "./SAChangeEnquiry.module.css";
 
 const SAChangeEnquiry = ({
-  open,
+  open: saOpen,
   handleClose,
   policyNo,
   TransactionNo,
 }: any) => {
-  const columns = [
-    {
-      field: "CompanyID",
-      header: "Company ID",
-      dbField: "company_id",
-    },
-    {
-      field: "PolicyID",
-      header: "Policy ID",
-      dbField: "policy_id",
-    },
-    {
-      field: "ClientID",
-      header: "Client ID",
-      dbField: "client_id",
-    },
-    {
-      field: "BenefitID",
-      header: "Benefit ID",
-      dbField: "benefit_id",
-    },
-    {
-      field: "BCoverage",
-      header: "BCoverage",
-      dbField: "b_coverage",
-    },
-    {
-      field: "BStartDate",
-      header: "BStartDate",
-      dbField: "b_start_date",
-      type: "date",
-    },
-    {
-      field: "BSumAssured",
-      header: "BSumAssured",
-      dbField: "b_sum_assured",
-    },
-    {
-      field: "BTerm",
-      header: "BTerm",
-      dbField: "b_term",
-    },
-    {
-      field: "BPTerm",
-      header: "BPTerm",
-      dbField: "bp_term",
-    },
-    {
-      field: "BPrem",
-      header: "BPrem",
-      dbField: "b_prem",
-    },
-    {
-      field: "NSumAssured",
-      header: "NSumAssured",
-      dbField: "n_sum_assured",
-    },
-    {
-      field: "NTerm",
-      header: "NTerm",
-      dbField: "n_term",
-    },
-    {
-      field: "NPTerm",
-      header: "NPTerm",
-      dbField: "np_term",
-    },
-    {
-      field: "NPrem",
-      header: "NPrem",
-      dbField: "n_prem",
-    },
-    {
-      field: "NAnnualPrem",
-      header: "NAnnualPrem",
-      dbField: "n_annual_prem",
-    },
-  ];
+  // const columns = [
+  //   {
+  //     field: "CompanyID",
+  //     header: "Company ID",
+  //     dbField: "company_id",
+  //   },
+  //   {
+  //     field: "PolicyID",
+  //     header: "Policy ID",
+  //     dbField: "policy_id",
+  //   },
+  //   {
+  //     field: "ClientID",
+  //     header: "Client ID",
+  //     dbField: "client_id",
+  //   },
+  //   {
+  //     field: "BenefitID",
+  //     header: "Benefit ID",
+  //     dbField: "benefit_id",
+  //   },
+  //   {
+  //     field: "BCoverage",
+  //     header: "BCoverage",
+  //     dbField: "b_coverage",
+  //   },
+  //   {
+  //     field: "BStartDate",
+  //     header: "BStartDate",
+  //     dbField: "b_start_date",
+  //     type: "date",
+  //   },
+  //   {
+  //     field: "BSumAssured",
+  //     header: "BSumAssured",
+  //     dbField: "b_sum_assured",
+  //   },
+  //   {
+  //     field: "BTerm",
+  //     header: "BTerm",
+  //     dbField: "b_term",
+  //   },
+  //   {
+  //     field: "BPTerm",
+  //     header: "BPTerm",
+  //     dbField: "bp_term",
+  //   },
+  //   {
+  //     field: "BPrem",
+  //     header: "BPrem",
+  //     dbField: "b_prem",
+  //   },
+  //   {
+  //     field: "NSumAssured",
+  //     header: "NSumAssured",
+  //     dbField: "n_sum_assured",
+  //   },
+  //   {
+  //     field: "NTerm",
+  //     header: "NTerm",
+  //     dbField: "n_term",
+  //   },
+  //   {
+  //     field: "NPTerm",
+  //     header: "NPTerm",
+  //     dbField: "np_term",
+  //   },
+  //   {
+  //     field: "NPrem",
+  //     header: "NPrem",
+  //     dbField: "n_prem",
+  //   },
+  //   {
+  //     field: "NAnnualPrem",
+  //     header: "NAnnualPrem",
+  //     dbField: "n_annual_prem",
+  //   },
+  // ];
 
   const [saChangeData, setsaChangeData] = useState([]);
   const getSAChangeData = () => {
@@ -108,29 +120,87 @@ const SAChangeEnquiry = ({
     getSAChangeData();
 
     return () => {};
-  }, [open]);
+  }, [saOpen]);
 
   return (
     <div>
-      <Modal show={open} onHide={handleClose} centered size="xl">
-        <Modal.Header closeButton>
-          <Modal.Title>{"SA Change"}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {
-            <div>
-              <form>
-                <EnquiryTable data={saChangeData} columns={columns} />
-              </form>
-            </div>
-          }
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <CustomModal
+        size="xl"
+        open={saOpen}
+        handleClose={handleClose}
+        title="Sum Assured change Details"
+      >
+        <TableContainer component={Paper}>
+          <Table aria-label="collapsible table">
+            <TableHead className={styles["table-head"]}>
+              <TableRow>
+                <TableCell className={styles.thstyle}>Policy Id</TableCell>
+                <TableCell className={styles.thstyle}>Client ID</TableCell>
+                <TableCell className={styles.thstyle}>Benefit ID</TableCell>
+                <TableCell className={styles.thstyle}>BCoverage</TableCell>
+                <TableCell className={styles.thstyle}>BStart Date</TableCell>
+                <TableCell className={styles.thstyle}>BSumAssured</TableCell>
+                <TableCell className={styles.thstyle}>BTerm</TableCell>
+                <TableCell className={styles.thstyle}>BPTerm</TableCell>
+                <TableCell className={styles.thstyle}>BPrem</TableCell>
+                <TableCell className={styles.thstyle}>NSumAssured</TableCell>
+                <TableCell className={styles.thstyle}>NTerm</TableCell>
+                <TableCell className={styles.thstyle}>NPTerm</TableCell>
+                <TableCell className={styles.thstyle}>NPrem</TableCell>
+                <TableCell className={styles.thstyle}>NAnnualPrem</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {saChangeData.map((data: any, index: number) => (
+                <TableRow
+                  key={data.PolicyID}
+                  style={{
+                    background: index % 2 === 0 ? "#f0f8ff" : "#d3e0ea", // Light blue for even rows, slightly darker blue for odd rows
+                    transition: "background 0.3s ease-in-out", // Add a smooth transition effect
+                  }}
+                >
+                  <TableCell
+                    className={styles.tdstyle}
+                    component="th"
+                    scope="row"
+                  >
+                    {data.PolicyID}
+                  </TableCell>
+                  <TableCell className={styles.tdstyle}>
+                    {data.ClientID}
+                  </TableCell>
+                  <TableCell className={styles.tdstyle}>
+                    {data.BenefitID}
+                  </TableCell>
+                  <TableCell>{data.BCoverage}</TableCell>
+                  <TableCell className={styles.tdstyle}>
+                    {moment(data.BStartDate).format("DD-MM-YYYY")}
+                  </TableCell>
+                  <TableCell className={styles.tdstyle}>
+                    {data.BSumAssured}
+                  </TableCell>
+                  <TableCell className={styles.tdstyle}>{data.BTerm}</TableCell>
+                  <TableCell className={styles.tdstyle}>
+                    {data.BPTerm}
+                  </TableCell>
+                  <TableCell className={styles.tdstyle}>{data.BPrem}</TableCell>
+                  <TableCell className={styles.tdstyle}>
+                    {data.NSumAssured}
+                  </TableCell>
+                  <TableCell className={styles.tdstyle}>{data.NTerm}</TableCell>
+                  <TableCell className={styles.tdstyle}>
+                    {data.NPTerm}
+                  </TableCell>
+                  <TableCell className={styles.tdstyle}>{data.NPrem}</TableCell>
+                  <TableCell className={styles.tdstyle}>
+                    {data.NAnnualPrem}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </CustomModal>
     </div>
   );
 };
