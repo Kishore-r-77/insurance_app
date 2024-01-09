cd1ba8075db9bbb649c3e0f913123e7ed84cd9d7
Kishore-r-77 <kishorework77@gmail.com>
2024-01-09 12:22:15 +0530

    fixed ToDate issue in extra

--- src/components/newBusiness/extras/extrasAdd.tsx
+++ src/components/newBusiness/extras/extrasAdd.tsx
@@ -80,7 +80,8 @@ function ExtrasAdd({
     setextraDetails((prev: any) =>
       prev.map((extra: any, index: number) => {
         if (index === i) {
-          return { ...extra, ToDate: date };
+          console.log(date, "wooooooo");
+          return { ...extra, ToDate: date.$d };
         } else return extra;
       })
     );
